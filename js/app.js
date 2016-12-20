ae26b8fc6abd5e9fdb593013015199a67f72bfdf
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-12-20 17:18:10 -0500

    created background image mixin

--- js/app.js
+++ js/app.js
@@ -5,15 +5,6 @@ $('#nav').onePageNav({
     scrollThreshold: 0.5,
     filter: '',
     easing: 'swing',
-    begin: function() {
-        //I get fired when the animation is starting
-    },
-    end: function() {
-        //I get fired when the animation is ending
-    },
-    scrollChange: function($currentListItem) {
-        //I get fired when you enter a section and I pass the list item of the section
-    }
 });
 
 $('.dropdown-toggle').dropdown();
